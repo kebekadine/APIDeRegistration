06fbcd24e919147fe2acf08a3bba2ac7ca9cbcab
kebekadine <49269060+kebekadine@users.noreply.github.com>
2021-12-02 15:52:42 +0100

    Update dbConfig.js

--- ServerSide/models/dbConfig.js
+++ ServerSide/models/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const userSchema = require('./userSchema');
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI| 'mongodb+srv://dataBaseUsr:dbUser@cluster0.wqkqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
     .then(() => {
         console.log("connected to the database");
     })
